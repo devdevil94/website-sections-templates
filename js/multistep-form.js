f6bb5e0237d82fddf91a5f54e02d909cbe0698a0
devdevil-production <saudd2gh@gmail.com>
2020-03-05 23:27:42 +0400

    Add update method

--- js/multistep-form.js
+++ js/multistep-form.js
@@ -1,24 +1,40 @@
-let step = 1;
+let stepNum = 1;
+let steps;
 
 const next = () => {
-  if (step === 3) return;
-  step++;
+  if (stepNum === 3) return;
+  stepNum++;
 };
 
 const back = () => {
-  if (step === 1) return;
-  step--;
+  if (stepNum === 1) return;
+  stepNum--;
+};
+
+const update = () => {
+  steps.forEach(step =>
+    {
+        if(step.classlist.contains("active")) 
+        step.classlist.remove("active")
+    }
+  );
+  const currentStep = steps[stepNum - 1];
+  currentStep.classlist.add("active");
 };
 
 const nextBtn = document.getElementsByClassName("next-btn")[0];
 const backBtn = document.getElementsByClassName("back-btn")[0];
 
+steps = document.querySelectorAll(".multistep-form .step");
+
 nextBtn.addEventListener("click", () => {
   next();
-  console.log("next , step:", step);
+  console.log("next , step:", stepNum);
+  update();
 });
 
 backBtn.addEventListener("click", () => {
   back();
-  console.log("back , step:", step);
+  console.log("back , step:", stepNum);
+  update();
 });
